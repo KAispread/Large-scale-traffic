c9dca5101337df6ba584c2a455a2cd77642accca
LeeKiWoo <pain0121@naver.com>
2025-09-11 00:24:39 +0900

    chapter-2.17: implement getting `ArticlePageResponse`

--- service/article/src/main/java/kuke/board/article/controller/ArticleController.java
+++ service/article/src/main/java/kuke/board/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kuke.board.article.controller;
 import kuke.board.article.service.ArticleService;
 import kuke.board.article.service.request.ArticleCreateRequest;
 import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticlePageResponse;
 import kuke.board.article.service.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping
@@ -23,6 +25,15 @@ public class ArticleController {
     @GetMapping("/v1/articles/{articleId}")
     public ArticleResponse read(@PathVariable Long articleId) {
         return articleService.read(articleId);
+    }
+
+    @GetMapping("/v1/articles")
+    public ArticlePageResponse readAll(
+        @RequestParam("boardId") Long boardId,
+        @RequestParam("page") Long page,
+        @RequestParam("pageSize") Long pageSize
+    ) {
+        return articleService.readAll(boardId, page, pageSize);
     }
 
     @PostMapping("/v1/articles")
--- service/article/src/main/java/kuke/board/article/repository/ArticleRepository.java
+++ service/article/src/main/java/kuke/board/article/repository/ArticleRepository.java
@@ -1,8 +1,44 @@
 package kuke.board.article.repository;
 
+import java.util.List;
 import kuke.board.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Query(
+        value = """
+                SELECT article.article_id, article.title, article.content, article.board_id, article.writer_id, article.created_at, article.modified_at
+                FROM (
+                    select article_id from article
+                    where board_id = :boardId
+                    order by article_id desc
+                    limit :limit offset :offset
+                ) t LEFT JOIN article ON t.article_id = article.article_id
+            """,
+        nativeQuery = true
+    )
+    List<Article> findAll(
+        @Param("boardId") Long boardId,
+        @Param("offset") Long offset,
+        @Param("limit") Long limit
+    );
+
+    @Query(
+        value = """
+                SELECT count(*)
+                FROM (
+                    select article_id from article
+                    where board_id = :boardId
+                    limit :limit
+                ) t
+            """,
+        nativeQuery = true
+    )
+    long count(
+        @Param("boardId") Long boardId,
+        @Param("limit") Long limit
+    );
 }
